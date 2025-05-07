309919a3038aeb43476082eb1e7843ceec523632
Azad Rashidov <azadlezgi@yandex.ru>
2025-05-07 18:30:15 +0400

    Update and rename config.php to config_dist.php

--- config_dist.php
+++ config_dist.php
@@ -1,12 +1,12 @@
 <?php
 // database settings
-//$db_host = "5.9.154.173";
 $db_host = "localhost";
-$db_user = "carsaz_new";
-$db_pass = "7K9a8O2sG3z6H2c9";
-$db_name = "carsaz_new";
+$db_user = "";
+$db_pass = "";
+$db_name = "";
 $db_prefix = "2cz11_";
 $db_charset = "utf8";
+
 define("DB_PREFIX", "2cz11_");
 define("COOKIE_PREFIX", "3HV9D_");
 ?>
